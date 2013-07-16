685b99fa79cdfa3486eb44b3c9da2d5b0c16c128
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-16 12:08:41 -0400

    Commenting advanced associations

--- app/models/superperson.rb
+++ app/models/superperson.rb
@@ -1,9 +1,10 @@
 class Superperson < ActiveRecord::Base
-
+  #Both sides of a self join where one Superperson can be the "group_leader" for several other Superpeople
   has_many :super_henchmen, class_name: 'Superperson', foreign_key: 'group_leader_id'
 
   belongs_to :group_leader, class_name: 'Superperson'
 
+  #Polymorphic Association where a Superperson has either a Supercat, Superdog or Superferret
   belongs_to :superpet, polymorphic: true
 
 
